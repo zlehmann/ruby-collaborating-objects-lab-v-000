e6d217b1c11b66bfd77678270940148500babe0c
Zachary Lehmann <zlehmann@gmail.com>
2018-09-30 22:35:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
@@ -18,10 +18,11 @@ class Song
   end
 
   def artist=(name)
-    Artist.find_or_create_by_name(name)
+    @artist = Artist.find_or_create_by_name(name)
   end
 
   def artist
+    @artist
   end
 
   def artist_name=(name)
