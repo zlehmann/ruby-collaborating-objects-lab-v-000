772ad539e29d8860ca08175ef09e571ec02c3bb9
Zachary Lehmann <zlehmann@gmail.com>
2018-09-30 20:10:50 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+  attr_accessor :path 
+
+  def initialization(path)
+    @path = path
+  end
+
+  def files
+
+  end
+
+  def import
+    files = Dir["#{path}*/.mp3"]
+  end
+
+end
