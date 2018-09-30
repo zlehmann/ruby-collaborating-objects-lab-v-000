e50edac5f0b31189e98ef50882ae4720a58fcf00
Zachary Lehmann <zlehmann@gmail.com>
2018-09-30 21:00:40 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,28 @@
 class Artist
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
 
   def add_song
     @songs << song
   end
+
+  def all 
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    @@all.each do |artist|
+      if artist.name == name
+        return artist
+      else
+        new_artist = Artist.new(name)
+      end
+    end
 
 end
