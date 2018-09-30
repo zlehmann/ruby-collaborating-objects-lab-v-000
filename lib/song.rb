2f25fbea5ec4008b71b223fb654e7248f207edba
Zachary Lehmann <zlehmann@gmail.com>
2018-09-30 21:21:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
     @name = name
   end
 
-  def new_by_filename(filename)
+  def self.new_by_filename(filename)
     @name = filename.split(" - ")[1]
     @artist = filename.split(" - ")[0]
     song = Song.new(song_name)
